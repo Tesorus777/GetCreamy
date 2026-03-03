49cd4396d3bbf6d9a2e69027f550e2d94cff4efa
Nicholas Tesoro <ntesoro777@gmail.com>
2026-03-02 16:05:31 -0800

    - Added Maintenance site to run during major upgrades - Changed _Layout.cshtml to have Date function for footer year

---
+++ IceCreamMaintenanceWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IceCreamMaintenanceWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
